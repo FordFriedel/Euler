faa6638012f55864a0f69c02c13396e2376b82c7
FordFriedel <ford.friedel@gmail.com>
2022-04-22 12:51:14 -0500

    7, 13

---
+++ p07.py
@@ -0,0 +1,22 @@
+#unsolved
+#4/22/22
+
+from math import sqrt
+
+primes = [2]
+run = True
+x = 3
+while len(primes) < 10001:
+    isPrime = True
+    for y in primes:
+        if(y >= sqrt(x)+1):
+            break
+        if(x%y == 0):
+            isPrime = False
+            break
+    if(isPrime):
+        primes.append(x)
+    x += 1
+print(primes[-1])
+
+#104743
