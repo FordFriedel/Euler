c2cf04330ec5e2e3326f5c6d668ede1fda548a37
FordFriedel <ford.friedel@gmail.com>
2022-05-09 12:35:21 -0500

    problem 15

--- p15.py
+++ p15.py
@@ -10,3 +10,30 @@
 # 3x3 = 20??
 #and 2x2 = 6
 
+
+def factorial(x: int):
+    "find the factorial of an integer"
+    if x < 1:
+        return 0
+    product = x
+    while x > 1:
+        x -= 1
+        product *= x
+    return product
+
+
+
+def lattice_square(x: int):
+    "find the number of possible permutations through a lattice square for a given width"
+    if x < 1:
+        return 0
+    permutations = factorial(2* x) / (factorial(2 * x - x) * factorial(x))
+    return int(permutations)
+
+if __name__ == "__main__":
+    print(lattice_square(1))
+    print(lattice_square(2))
+    print(lattice_square(3))
+    print(lattice_square(4))
+    print(lattice_square(20))
+
